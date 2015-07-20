f84f23bcc284c39987362ee5a1548bb5bd17f4fa
Jorgen Thelin <github@thelin.me>
2015-07-19 20:37:03 -0700

    Tweets @ 2015-07-19 - Updated with grailbird_updater

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,6 @@
-var payload_details =  {
-  "tweets" : 337,
-  "created_at" : "2015-07-17 11:47:19 +0000",
-  "lang" : "en"
+var payload_details = {
+  "tweets": 340,
+  "created_at": "2015-07-17 11:47:19 +0000",
+  "lang": "en",
+  "updated_at": "Mon Jul 20 03:31:33 +0000 2015"
 }
