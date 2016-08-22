971571bc1d7095989beaccc448cd1108f71d24de
Jorgen Thelin <jthelin@microsoft.com>
2016-08-21 23:55:08 -0700

    Tweets 2016-08-21

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 992,
+  "tweets": 999,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Sat Aug 13 05:55:07 +0000 2016"
+  "updated_at": "Mon Aug 22 06:55:08 +0000 2016"
 }
--- tweets/data/js/tweets/2016_08.js
+++ tweets/data/js/tweets/2016_08.js
@@ -1,5 +1,543 @@
 Grailbird.data.tweets_2016_08 = [
   {
+    "created_at": "Sat Aug 20 21:14:16 +0000 2016",
+    "id": 767107507646980096,
+    "id_str": "767107507646980096",
+    "text": "Is Pokémon Go the real dawn of location-based marketing? https://t.co/xPSqQMPiet",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/xPSqQMPiet",
+          "expanded_url": "http://www.cmo.com/features/articles/2016/7/14/big-brands-missing-the-pokemon-go-craze.html#gs.null",
+          "display_url": "cmo.com/features/artic…",
+          "indices": [
+            57,
+            80
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Thu Aug 18 16:44:20 +0000 2016",
+    "id": 766314802201235456,
+    "id_str": "766314802201235456",
+    "text": "Time to revisit assumptions when \"the greatest experiment in monetary policy in the history of the world\" not worked https://t.co/wgLcT6i9aY",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/wgLcT6i9aY",
+          "expanded_url": "https://apple.news/AK00YfPXWN7eML0nA-_HfnQ",
+          "display_url": "apple.news/AK00YfPXWN7eML…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 2,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Sun Aug 14 21:42:26 +0000 2016",
+    "id": 764940269829574657,
+    "id_str": "764940269829574657",
+    "text": "Army heeds Obama call, attacks high energy bills https://t.co/pK9v2Lbx07",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/pK9v2Lbx07",
+          "expanded_url": "https://apple.news/Arrv6bM-gQW2iXNth4n8zww",
+          "display_url": "apple.news/Arrv6bM-gQW2iX…",
+          "indices": [
+            49,
+            72
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 2,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Sun Aug 14 21:39:38 +0000 2016",
+    "id": 764939563735224320,
+    "id_str": "764939563735224320",
+    "text": "Equity: how fictional tales of women on Wall Street shed light on reality - the guardian https://t.co/VSWlxmuT3u",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/VSWlxmuT3u",
+          "expanded_url": "https://apple.news/AQ5Mgic8iRlCxZ_dOMASGpQ",
+          "display_url": "apple.news/AQ5Mgic8iRlCxZ…",
+          "indices": [
+            89,
+            112
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Sun Aug 14 18:36:06 +0000 2016",
+    "id": 764893376156082176,
+    "id_str": "764893376156082176",
+    "text": "Package Management Basics: apt, yum, dnf, pkg - good summary from @DigitalOcean https://t.co/o4DJFlWoei",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "digitalocean",
+          "name": "DigitalOcean",
+          "id": 457033547,
+          "id_str": "457033547",
+          "indices": [
+            66,
+            79
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/o4DJFlWoei",
+          "expanded_url": "https://www.digitalocean.com/community/tutorials/package-management-basics-apt-yum-dnf-pkg?utm_medium=social&utm_source=twitter&utm_campaign=apt_yum_ubuntu_tut&utm_content=image",
+          "display_url": "digitalocean.com/community/tuto…",
+          "indices": [
+            80,
+            103
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Sun Aug 14 05:54:41 +0000 2016",
+    "id": 764701760506048512,
+    "id_str": "764701760506048512",
+    "text": "RT @WhiteHouse: \"We've set the first-ever national standards limiting the amount of carbon pollution power plants\" can emit —@POTUS: https:…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "WhiteHouse",
+          "name": "The White House",
+          "id": 30313925,
+          "id_str": "30313925",
+          "indices": [
+            3,
+            14
+          ]
+        },
+        {
+          "screen_name": "POTUS",
+          "name": "President Obama",
+          "id": 1536791610,
+          "id_str": "1536791610",
+          "indices": [
+            125,
+            131
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/jCMTjXEnSQ",
+          "expanded_url": "http://snpy.tv/2b9uT2E",
+          "display_url": "snpy.tv/2b9uT2E",
+          "indices": [
+            139,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Sat Aug 13 21:25:07 +0000 2016",
+      "id": 764573523713011713,
+      "id_str": "764573523713011713",
+      "text": "\"We've set the first-ever national standards limiting the amount of carbon pollution power plants\" can emit —@POTUS: https://t.co/jCMTjXEnSQ",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+          {
+            "screen_name": "POTUS",
+            "name": "President Obama",
+            "id": 1536791610,
+            "id_str": "1536791610",
+            "indices": [
+              109,
+              115
+            ]
+          }
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/jCMTjXEnSQ",
+            "expanded_url": "http://snpy.tv/2b9uT2E",
+            "display_url": "snpy.tv/2b9uT2E",
+            "indices": [
+              117,
+              140
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 30313925,
+        "id_str": "30313925",
+        "name": "The White House",
+        "screen_name": "WhiteHouse",
+        "location": "Washington, DC",
+        "description": "Follow for the latest from President Obama and his Administration. For tweets from the President, follow @POTUS. Tweets may be archived: https://t.co/eVVzoBb3Zr",
+        "url": "http://t.co/NgUd6eRXCR",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "http://t.co/NgUd6eRXCR",
+                "expanded_url": "http://whitehouse.gov",
+                "display_url": "whitehouse.gov",
+                "indices": [
+                  0,
+                  22
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+              {
+                "url": "https://t.co/eVVzoBb3Zr",
+                "expanded_url": "http://wh.gov/privacy",
+                "display_url": "wh.gov/privacy",
+                "indices": [
+                  137,
+                  160
+                ]
+              }
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 11282816,
+        "friends_count": 224,
+        "listed_count": 66516,
+        "created_at": "Fri Apr 10 21:10:30 +0000 2009",
+        "favourites_count": 0,
+        "utc_offset": -14400,
+        "time_zone": "Eastern Time (US & Canada)",
+        "geo_enabled": false,
+        "verified": true,
+        "statuses_count": 26177,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": true,
+        "profile_background_color": "D0CFCB",
+        "profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000013910263/9426106939082a6e8935fc31fe5efd31.jpeg",
+        "profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000013910263/9426106939082a6e8935fc31fe5efd31.jpeg",
+        "profile_background_tile": true,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/747463206759698432/c8cVVWhj_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/747463206759698432/c8cVVWhj_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/30313925/1467725763",
+        "profile_link_color": "336699",
+        "profile_sidebar_border_color": "FFFFFF",
+        "profile_sidebar_fill_color": "E1E8EE",
+        "profile_text_color": "666666",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 664,
+      "favorite_count": 2043,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 664,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Sat Aug 13 20:47:06 +0000 2016",
+    "id": 764563957034000384,
+    "id_str": "764563957034000384",
+    "text": "Why Vaporeon dominates gym defense in 'Pokémon Go'  https://t.co/sIgcWHfaM5",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/sIgcWHfaM5",
+          "expanded_url": "https://www.thrillist.com/news/nation/how-to-beat-vaporeon-in-pokemon-go",
+          "display_url": "thrillist.com/news/nation/ho…",
+          "indices": [
+            52,
+            75
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "fr"
+  },
+  {
     "created_at": "Sat Aug 13 05:09:39 +0000 2016",
     "id": 764328039127801856,
     "id_str": "764328039127801856",
