e4ae2ebc07f4ba0f002ba3ae8eecffdb8e4a4d00
Jorgen Thelin <jthelin@microsoft.com>
2016-09-01 23:55:04 -0700

    Tweets 2016-09-01

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -2,5 +2,5 @@ var payload_details = {
   "tweets": 1012,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Fri Sep 02 06:47:29 +0000 2016"
+  "updated_at": "Fri Sep 02 06:55:04 +0000 2016"
 }
