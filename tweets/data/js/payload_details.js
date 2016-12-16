55d96cf326d3b725cd89170f5f1f49ddb4811b8e
Jorgen Thelin <jthelin@microsoft.com>
2016-12-15 19:24:50 -0800

    Tweets 2016-12-15

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 1079,
+  "tweets": 1083,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Sat Dec 03 07:24:59 +0000 2016"
+  "updated_at": "Fri Dec 16 03:24:50 +0000 2016"
 }
---
+++ tweets/data/js/tweets/2016_12.js
@@ -0,0 +1,585 @@
+Grailbird.data.tweets_2016_12 = [
+  {
+    "created_at": "Thu Dec 08 19:21:16 +0000 2016",
+    "id": 806941733565009920,
+    "id_str": "806941733565009920",
+    "text": "RT @terrajobst: Want to know if code is compatible with .NET Standard? Check out my third video. https://t.co/Eo7BmKWZcP",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "terrajobst",
+          "name": "Immo Landwerth",
+          "id": 512620228,
+          "id_str": "512620228",
+          "indices": [
+            3,
+            14
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/Eo7BmKWZcP",
+          "expanded_url": "https://youtu.be/rzs_FGPyAlY",
+          "display_url": "youtu.be/rzs_FGPyAlY",
+          "indices": [
+            97,
+            120
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Wed Dec 07 16:24:34 +0000 2016",
+      "id": 806534877558378497,
+      "id_str": "806534877558378497",
+      "text": "Want to know if code is compatible with .NET Standard? Check out my third video. https://t.co/Eo7BmKWZcP",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/Eo7BmKWZcP",
+            "expanded_url": "https://youtu.be/rzs_FGPyAlY",
+            "display_url": "youtu.be/rzs_FGPyAlY",
+            "indices": [
+              81,
+              104
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 512620228,
+        "id_str": "512620228",
+        "name": "Immo Landwerth",
+        "screen_name": "terrajobst",
+        "location": "Redmond, WA",
+        "description": "Program manager on .NET at Microsoft. Opinions are my own.",
+        "url": "https://t.co/pfw9pKc4sL",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/pfw9pKc4sL",
+                "expanded_url": "http://immo.landwerth.net",
+                "display_url": "immo.landwerth.net",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 5202,
+        "friends_count": 332,
+        "listed_count": 185,
+        "created_at": "Sat Mar 03 00:48:21 +0000 2012",
+        "favourites_count": 6032,
+        "utc_offset": -28800,
+        "time_zone": "Pacific Time (US & Canada)",
+        "geo_enabled": true,
+        "verified": false,
+        "statuses_count": 10868,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "C0DEED",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/615528062449364992/Z_r77yeG_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/615528062449364992/Z_r77yeG_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/512620228/1379201221",
+        "profile_link_color": "1DA1F2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": true,
+        "default_profile": true,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 67,
+      "favorite_count": 117,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 67,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Thu Dec 08 18:25:27 +0000 2016",
+    "id": 806927689936355328,
+    "id_str": "806927689936355328",
+    "text": "Microsoft Research gets +8 new ACM Fellows and +5 Distinguished Members. Congrats to my co-workers in B99 and beyond!https://t.co/okYxpEmZkP",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/okYxpEmZkP",
+          "expanded_url": "http://blogs.microsoft.com/next/2016/12/08/microsoft-researchers-earn-distinctions-premier-computing-society/",
+          "display_url": "blogs.microsoft.com/next/2016/12/0…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Mon Dec 05 22:52:30 +0000 2016",
+    "id": 805907728786825216,
+    "id_str": "805907728786825216",
+    "text": "RT @richorama: The next Orleans virtual meetup is on Thursday: https://t.co/iF9AKcW1y1",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "richorama",
+          "name": "Richard Astbury",
+          "id": 21290781,
+          "id_str": "21290781",
+          "indices": [
+            3,
+            13
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/iF9AKcW1y1",
+          "expanded_url": "https://github.com/OrleansContrib/meetups#meetup-12-deploying-orleans-with-jakub-konecki-and-the-orleans-core-team",
+          "display_url": "github.com/OrleansContrib…",
+          "indices": [
+            63,
+            86
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Mon Dec 05 11:50:30 +0000 2016",
+      "id": 805741130264494080,
+      "id_str": "805741130264494080",
+      "text": "The next Orleans virtual meetup is on Thursday: https://t.co/iF9AKcW1y1",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/iF9AKcW1y1",
+            "expanded_url": "https://github.com/OrleansContrib/meetups#meetup-12-deploying-orleans-with-jakub-konecki-and-the-orleans-core-team",
+            "display_url": "github.com/OrleansContrib…",
+            "indices": [
+              48,
+              71
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 21290781,
+        "id_str": "21290781",
+        "name": "Richard Astbury",
+        "screen_name": "richorama",
+        "location": "Woodbridge, Suffolk, UK",
+        "description": "Cloud computing guy who loves node, c# & python. Pluralsight author. Microsoft MVP for #Azure. Works at @two10degrees",
+        "url": "https://t.co/gyXs0gtOPy",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/gyXs0gtOPy",
+                "expanded_url": "https://richorama.github.io",
+                "display_url": "richorama.github.io",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 1047,
+        "friends_count": 548,
+        "listed_count": 75,
+        "created_at": "Thu Feb 19 11:27:11 +0000 2009",
+        "favourites_count": 683,
+        "utc_offset": 0,
+        "time_zone": "London",
+        "geo_enabled": true,
+        "verified": false,
+        "statuses_count": 6434,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "333333",
+        "profile_background_image_url": "http://pbs.twimg.com/profile_background_images/601675027/6d7xvz3z53te1k1b8cs3.png",
+        "profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/601675027/6d7xvz3z53te1k1b8cs3.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/704661140014555138/QP3Qp1Us_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/704661140014555138/QP3Qp1Us_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/21290781/1481287032",
+        "profile_link_color": "1B95E0",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "EFEFEF",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": true,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 7,
+      "favorite_count": 9,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 7,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Mon Dec 05 22:50:34 +0000 2016",
+    "id": 805907244499943424,
+    "id_str": "805907244499943424",
+    "text": "RT @msftorleans: Technical Preview of 2.0 with support for .NET Core has been published on https://t.co/yDCgEvrCrN. Details in https://t.co…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "msftorleans",
+          "name": "Microsoft Orleans",
+          "id": 3366266218,
+          "id_str": "3366266218",
+          "indices": [
+            3,
+            15
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/yDCgEvrCrN",
+          "expanded_url": "http://dotnet.myget.org",
+          "display_url": "dotnet.myget.org",
+          "indices": [
+            91,
+            114
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Mon Dec 05 19:58:16 +0000 2016",
+      "id": 805863884007424000,
+      "id_str": "805863884007424000",
+      "text": "Technical Preview of 2.0 with support for .NET Core has been published on https://t.co/yDCgEvrCrN. Details in https://t.co/4jASQ8CdLA",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/yDCgEvrCrN",
+            "expanded_url": "http://dotnet.myget.org",
+            "display_url": "dotnet.myget.org",
+            "indices": [
+              74,
+              97
+            ]
+          },
+          {
+            "url": "https://t.co/4jASQ8CdLA",
+            "expanded_url": "https://blogs.msdn.microsoft.com/orleans/2016/12/05/orleans-20-tech-preview-net-core/",
+            "display_url": "blogs.msdn.microsoft.com/orleans/2016/1…",
+            "indices": [
+              110,
+              133
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://www.tweetiumapp.com\" rel=\"nofollow\">Tweetium for Windows</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 3366266218,
+        "id_str": "3366266218",
+        "name": "Microsoft Orleans",
+        "screen_name": "msftorleans",
+        "location": "All around the world",
+        "description": "Official account for Microsoft Orleans. Orleans is a framework that provides a straightforward approach to building distributed high-scale applications.",
+        "url": "https://t.co/l66ZAwxNBp",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/l66ZAwxNBp",
+                "expanded_url": "https://github.com/dotnet/orleans/",
+                "display_url": "github.com/dotnet/orleans/",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 702,
+        "friends_count": 0,
+        "listed_count": 12,
+        "created_at": "Wed Jul 08 16:52:41 +0000 2015",
+        "favourites_count": 0,
+        "utc_offset": -28800,
+        "time_zone": "Pacific Time (US & Canada)",
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 84,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "000000",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/787522221405118464/_30RoZBf_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/787522221405118464/_30RoZBf_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/3366266218/1437068035",
+        "profile_link_color": "ABB8C2",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "000000",
+        "profile_text_color": "000000",
+        "profile_use_background_image": false,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 32,
+      "favorite_count": 37,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 32,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "en"
+  }
+]
