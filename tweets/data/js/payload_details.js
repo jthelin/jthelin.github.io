45906208de7b53cf91ef4369246345949029c314
Jorgen Thelin <jthelin@microsoft.com>
2017-05-09 18:25:01 -0700

    Tweets 2017-05-09

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 1154,
+  "tweets": 1155,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Sun May 07 06:00:19 +0000 2017"
+  "updated_at": "Wed May 10 01:25:01 +0000 2017"
 }
--- tweets/data/js/tweets/2017_05.js
+++ tweets/data/js/tweets/2017_05.js
@@ -1,5 +1,60 @@
 Grailbird.data.tweets_2017_05 = [
   {
+    "created_at": "Tue May 09 21:56:56 +0000 2017",
+    "id": 862063866523537408,
+    "id_str": "862063866523537408",
+    "text": "Enterprise hard disks are faster and use more power, but are they more reliable? - Ars Technica https://t.co/0ZOdt64x13",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/0ZOdt64x13",
+          "expanded_url": "https://apple.news/AG1JmAM6pRsWJvTBrZQu00g",
+          "display_url": "apple.news/AG1JmAM6pRsWJv…",
+          "indices": [
+            96,
+            119
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
     "created_at": "Thu May 04 19:29:22 +0000 2017",
     "id": 860214790760747009,
     "id_str": "860214790760747009",
