86fc4fbc1ed6ad0ea5429e2b1163d4516681dfb1
Jorgen Thelin <jthelin@microsoft.com>
2017-07-19 23:44:39 -0700

    Tweets 2017-07-19

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 1207,
+  "tweets": 1210,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Sat Jul 08 05:30:51 +0000 2017"
+  "updated_at": "Thu Jul 20 06:44:39 +0000 2017"
 }
--- tweets/data/js/tweets/2017_07.js
+++ tweets/data/js/tweets/2017_07.js
@@ -1,5 +1,414 @@
 Grailbird.data.tweets_2017_07 = [
   {
+    "created_at": "Wed Jul 19 22:51:22 +0000 2017",
+    "id": 887807101019410432,
+    "id_str": "887807101019410432",
+    "text": "RT @nytimes: An ad showed a girl growing up to be a ballerina and a boy becoming a mathematician. Britain decided it had enough. https://t.…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "nytimes",
+          "name": "The New York Times",
+          "id": 807095,
+          "id_str": "807095",
+          "indices": [
+            3,
+            11
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Wed Jul 19 19:48:02 +0000 2017",
+      "id": 887760965219037184,
+      "id_str": "887760965219037184",
+      "text": "An ad showed a girl growing up to be a ballerina and a boy becoming a mathematician. Britain decided it had enough. https://t.co/ALqgKFNEbd",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/ALqgKFNEbd",
+            "expanded_url": "http://nyti.ms/2trj0z1",
+            "display_url": "nyti.ms/2trj0z1",
+            "indices": [
+              116,
+              139
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://www.socialflow.com\" rel=\"nofollow\">SocialFlow</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 807095,
+        "id_str": "807095",
+        "name": "The New York Times",
+        "screen_name": "nytimes",
+        "location": "New York City",
+        "description": "Where the conversation begins. Follow for breaking news, special reports, RTs of our journalists and more from https://t.co/YapuoqX0HS.",
+        "url": "http://t.co/ahvuWqicF9",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "http://t.co/ahvuWqicF9",
+                "expanded_url": "http://www.nytimes.com/",
+                "display_url": "nytimes.com",
+                "indices": [
+                  0,
+                  22
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+              {
+                "url": "https://t.co/YapuoqX0HS",
+                "expanded_url": "http://NYTimes.com",
+                "display_url": "NYTimes.com",
+                "indices": [
+                  111,
+                  134
+                ]
+              }
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 38662435,
+        "friends_count": 889,
+        "listed_count": 189817,
+        "created_at": "Fri Mar 02 20:41:42 +0000 2007",
+        "favourites_count": 15157,
+        "utc_offset": -14400,
+        "time_zone": "Eastern Time (US & Canada)",
+        "geo_enabled": false,
+        "verified": true,
+        "statuses_count": 285006,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": true,
+        "profile_background_color": "131516",
+        "profile_background_image_url": "http://pbs.twimg.com/profile_background_images/736339684/948f072cc2da4e3a5e9f2ebfb3b1a0e7.png",
+        "profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/736339684/948f072cc2da4e3a5e9f2ebfb3b1a0e7.png",
+        "profile_background_tile": true,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/856562742563663872/cl1sRx2G_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/856562742563663872/cl1sRx2G_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/807095/1488301156",
+        "profile_link_color": "607696",
+        "profile_sidebar_border_color": "FFFFFF",
+        "profile_sidebar_fill_color": "EFEFEF",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 543,
+      "favorite_count": 1429,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 543,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "lang": "en"
+  },
+  {
+    "created_at": "Wed Jul 19 22:48:49 +0000 2017",
+    "id": 887806461459349505,
+    "id_str": "887806461459349505",
+    "text": "RT @TechCrunch: The questions almost no startup founders ask https://t.co/QGNMkRPBg5",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "TechCrunch",
+          "name": "TechCrunch",
+          "id": 816653,
+          "id_str": "816653",
+          "indices": [
+            3,
+            14
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/QGNMkRPBg5",
+          "expanded_url": "http://tcrn.ch/2tef9RI",
+          "display_url": "tcrn.ch/2tef9RI",
+          "indices": [
+            61,
+            84
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Wed Jul 19 19:50:01 +0000 2017",
+      "id": 887761463879839746,
+      "id_str": "887761463879839746",
+      "text": "The questions almost no startup founders ask https://t.co/QGNMkRPBg5",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/QGNMkRPBg5",
+            "expanded_url": "http://tcrn.ch/2tef9RI",
+            "display_url": "tcrn.ch/2tef9RI",
+            "indices": [
+              45,
+              68
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://www.socialflow.com\" rel=\"nofollow\">SocialFlow</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 816653,
+        "id_str": "816653",
+        "name": "TechCrunch",
+        "screen_name": "TechCrunch",
+        "location": "San Francisco, CA",
+        "description": "Breaking technology news, analysis, and opinions from TechCrunch. The number one guide for all things tech. Got a tip? Let us know tips@techcrunch.com",
+        "url": "https://t.co/b5Oyx1k1ye",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/b5Oyx1k1ye",
+                "expanded_url": "http://techcrunch.com",
+                "display_url": "techcrunch.com",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 9407970,
+        "friends_count": 946,
+        "listed_count": 109298,
+        "created_at": "Wed Mar 07 01:27:09 +0000 2007",
+        "favourites_count": 2216,
+        "utc_offset": -25200,
+        "time_zone": "Pacific Time (US & Canada)",
+        "geo_enabled": true,
+        "verified": true,
+        "statuses_count": 161022,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": true,
+        "profile_background_color": "149500",
+        "profile_background_image_url": "http://pbs.twimg.com/profile_background_images/621096023751004161/BAKy7hCT.png",
+        "profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/621096023751004161/BAKy7hCT.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/879359733936701440/sitcq7wY_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/879359733936701440/sitcq7wY_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/816653/1500405732",
+        "profile_link_color": "097000",
+        "profile_sidebar_border_color": "FFFFFF",
+        "profile_sidebar_fill_color": "DDFFCC",
+        "profile_text_color": "222222",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 22,
+      "favorite_count": 52,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 22,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Wed Jul 19 19:50:14 +0000 2017",
+    "id": 887761518040817664,
+    "id_str": "887761518040817664",
+    "text": "Long term solution to tech industry recruitment diversity issues start at High School level &amp; forward. @tealsk12org https://t.co/MXJVNRG5kE",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "tealsk12org",
+          "name": "TEALS",
+          "id": 3240992597,
+          "id_str": "3240992597",
+          "indices": [
+            107,
+            119
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/MXJVNRG5kE",
+          "expanded_url": "https://www.geekwire.com/2017/women-minorities-shatter-records-tens-thousands-take-ap-computer-science-exams/",
+          "display_url": "geekwire.com/2017/women-min…",
+          "indices": [
+            120,
+            143
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 2,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
     "created_at": "Fri Jul 07 22:40:48 +0000 2017",
     "id": 883455787204395008,
     "id_str": "883455787204395008",
