70fcb47d481e5c11fe8752aca95cb236ee6bee27
Jorgen Thelin <jthelin@microsoft.com>
2017-11-06 08:30:21 -0800

    Tweets 2017-11-06

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 1294,
+  "tweets": 1297,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Thu Nov 02 00:20:26 +0000 2017"
+  "updated_at": "Mon Nov 06 16:30:21 +0000 2017"
 }
--- tweets/data/js/tweets/2017_11.js
+++ tweets/data/js/tweets/2017_11.js
@@ -1,5 +1,574 @@
 Grailbird.data.tweets_2017_11 = [
   {
+    "created_at": "Sat Nov 04 07:57:28 +0000 2017",
+    "id": 926720035367698433,
+    "id_str": "926720035367698433",
+    "text": "RT @uncle_dallas: How to Build a #kubernetes #Cluster with #ARM #RaspberryPi then run #dotnetCore on OpenFaas https://t.co/lLZPRrmk7W by @s…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+        {
+          "text": "kubernetes",
+          "indices": [
+            33,
+            44
+          ]
+        },
+        {
+          "text": "Cluster",
+          "indices": [
+            45,
+            53
+          ]
+        },
+        {
+          "text": "ARM",
+          "indices": [
+            59,
+            63
+          ]
+        },
+        {
+          "text": "RaspberryPi",
+          "indices": [
+            64,
+            76
+          ]
+        },
+        {
+          "text": "dotnetCore",
+          "indices": [
+            86,
+            97
+          ]
+        }
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "uncle_dallas",
+          "name": "Marco Dalla Libera",
+          "id": 404190338,
+          "id_str": "404190338",
+          "indices": [
+            3,
+            16
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/lLZPRrmk7W",
+          "expanded_url": "https://www.hanselman.com/blog/HowToBuildAKubernetesClusterWithARMRaspberryPiThenRunNETCoreOnOpenFaas.aspx",
+          "display_url": "hanselman.com/blog/HowToBuil…",
+          "indices": [
+            110,
+            133
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Fri Nov 03 15:53:00 +0000 2017",
+      "id": 926477318616260608,
+      "id_str": "926477318616260608",
+      "text": "How to Build a #kubernetes #Cluster with #ARM #RaspberryPi then run #dotnetCore on OpenFaas https://t.co/lLZPRrmk7W… https://t.co/UKsiJsoQaA",
+      "truncated": true,
+      "entities": {
+        "hashtags": [
+          {
+            "text": "kubernetes",
+            "indices": [
+              15,
+              26
+            ]
+          },
+          {
+            "text": "Cluster",
+            "indices": [
+              27,
+              35
+            ]
+          },
+          {
+            "text": "ARM",
+            "indices": [
+              41,
+              45
+            ]
+          },
+          {
+            "text": "RaspberryPi",
+            "indices": [
+              46,
+              58
+            ]
+          },
+          {
+            "text": "dotnetCore",
+            "indices": [
+              68,
+              79
+            ]
+          }
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/lLZPRrmk7W",
+            "expanded_url": "https://www.hanselman.com/blog/HowToBuildAKubernetesClusterWithARMRaspberryPiThenRunNETCoreOnOpenFaas.aspx",
+            "display_url": "hanselman.com/blog/HowToBuil…",
+            "indices": [
+              92,
+              115
+            ]
+          },
+          {
+            "url": "https://t.co/UKsiJsoQaA",
+            "expanded_url": "https://twitter.com/i/web/status/926477318616260608",
+            "display_url": "twitter.com/i/web/status/9…",
+            "indices": [
+              117,
+              140
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 404190338,
+        "id_str": "404190338",
+        "name": "Marco Dalla Libera",
+        "screen_name": "uncle_dallas",
+        "location": "Como",
+        "description": "Freelance Consultant, #Aspnet & #Xamarin Dev, Trainer, Team & Project Manager, @Nopcommerce Expert. Opinions are my own. Retweets & likes are not endorsements.",
+        "url": "http://t.co/Lf57wsvoBL",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "http://t.co/Lf57wsvoBL",
+                "expanded_url": "http://www.dallalibera.net",
+                "display_url": "dallalibera.net",
+                "indices": [
+                  0,
+                  22
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 1047,
+        "friends_count": 976,
+        "listed_count": 239,
+        "created_at": "Thu Nov 03 15:36:30 +0000 2011",
+        "favourites_count": 1177,
+        "utc_offset": 3600,
+        "time_zone": "Europe/Rome",
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 4528,
+        "lang": "it",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "000000",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/774199936971866112/JHiaSCxS_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/774199936971866112/JHiaSCxS_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/404190338/1395697732",
+        "profile_link_color": "FAB81E",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "000000",
+        "profile_text_color": "000000",
+        "profile_use_background_image": false,
+        "has_extended_profile": true,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 25,
+      "favorite_count": 82,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 25,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Fri Nov 03 22:10:40 +0000 2017",
+    "id": 926572361251405824,
+    "id_str": "926572361251405824",
+    "text": "RT @adriancolyer: Log20 knows exactly where you should place those logging statements for maximum information at low overhead: https://t.co…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "adriancolyer",
+          "name": "Adrian Colyer",
+          "id": 23971403,
+          "id_str": "23971403",
+          "indices": [
+            3,
+            16
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Fri Nov 03 08:31:01 +0000 2017",
+      "id": 926366088392257536,
+      "id_str": "926366088392257536",
+      "text": "Log20 knows exactly where you should place those logging statements for maximum information at low overhead: https://t.co/WYrNwMgQQ5",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/WYrNwMgQQ5",
+            "expanded_url": "http://blog.acolyer.org/2017/11/03/log20-fully-automated-optimal-placement-of-log-printing-statements-under-specified-overhead-threshold",
+            "display_url": "blog.acolyer.org/2017/11/03/log…",
+            "indices": [
+              109,
+              132
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 23971403,
+        "id_str": "23971403",
+        "name": "Adrian Colyer",
+        "screen_name": "adriancolyer",
+        "location": "Romsey, England",
+        "description": "Author: #themorningpaper, Venture Partner with Accel Partners, London after many years as CTO for SpringSource and then apps at VMware, Pivotal",
+        "url": "https://t.co/b74UImWlMg",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/b74UImWlMg",
+                "expanded_url": "http://www.accel.com/team/venture-development/london/adrian-colyer",
+                "display_url": "accel.com/team/venture-d…",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 15749,
+        "friends_count": 308,
+        "listed_count": 649,
+        "created_at": "Thu Mar 12 15:51:34 +0000 2009",
+        "favourites_count": 2336,
+        "utc_offset": 0,
+        "time_zone": "London",
+        "geo_enabled": true,
+        "verified": false,
+        "statuses_count": 4707,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "000000",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/902829228990709760/mWT4DjHt_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/902829228990709760/mWT4DjHt_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/23971403/1412244432",
+        "profile_link_color": "C6A168",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "000000",
+        "profile_text_color": "000000",
+        "profile_use_background_image": false,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 5,
+      "favorite_count": 46,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 5,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "lang": "en"
+  },
+  {
+    "created_at": "Fri Nov 03 22:10:03 +0000 2017",
+    "id": 926572205592297472,
+    "id_str": "926572205592297472",
+    "text": "RT @iamdevloper: 1969:\n-what're you doing with that 2KB of RAM?\n-sending people to the moon\n\n2017:\n-what're you doing with that 1.5GB of RA…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "iamdevloper",
+          "name": "I Am Devloper",
+          "id": 564919357,
+          "id_str": "564919357",
+          "indices": [
+            3,
+            15
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Fri Nov 03 14:38:15 +0000 2017",
+      "id": 926458505355235328,
+      "id_str": "926458505355235328",
+      "text": "1969:\n-what're you doing with that 2KB of RAM?\n-sending people to the moon\n\n2017:\n-what're you doing with that 1.5GB of RAM?\n-running Slack",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+
+        ]
+      },
+      "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 564919357,
+        "id_str": "564919357",
+        "name": "I Am Devloper",
+        "screen_name": "iamdevloper",
+        "location": "Localhost",
+        "description": "Contact adeveloper1@gmail or DM if you want me to post your job listing/link to your product for 💰\n\nAnalytics/impressions on request 👀",
+        "url": "http://t.co/Hcd0PBjb",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "http://t.co/Hcd0PBjb",
+                "expanded_url": "http://www.urbandictionary.com/define.php?term=RTFM",
+                "display_url": "urbandictionary.com/define.php?ter…",
+                "indices": [
+                  0,
+                  20
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 232035,
+        "friends_count": 0,
+        "listed_count": 3712,
+        "created_at": "Fri Apr 27 21:06:51 +0000 2012",
+        "favourites_count": 0,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 4321,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "131516",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+        "profile_background_tile": true,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/477397164453527552/uh2w1u1o_normal.jpeg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/477397164453527552/uh2w1u1o_normal.jpeg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/564919357/1505113513",
+        "profile_link_color": "009999",
+        "profile_sidebar_border_color": "EEEEEE",
+        "profile_sidebar_fill_color": "EFEFEF",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 28999,
+      "favorite_count": 63807,
+      "favorited": true,
+      "retweeted": true,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 28999,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "lang": "en"
+  },
+  {
     "created_at": "Wed Nov 01 22:36:01 +0000 2017",
     "id": 925853964398092288,
     "id_str": "925853964398092288",
