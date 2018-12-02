16f6ea99b1ee358ff38e122bf8c411aefa8bc419
Jorgen Thelin <jthelin@microsoft.com>
2018-12-02 14:35:56 -0800

    Tweets 2018-12-02

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 1558,
+  "tweets": 1559,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Fri Nov 30 03:25:48 +0000 2018"
+  "updated_at": "Sun Dec 02 22:35:56 +0000 2018"
 }
---
+++ tweets/data/js/tweets/2018_12.js
@@ -0,0 +1,57 @@
+Grailbird.data.tweets_2018_12 = [
+  {
+    "created_at": "Sat Dec 01 20:11:12 +0000 2018",
+    "id": 1068960727711408128,
+    "id_str": "1068960727711408128",
+    "text": "Must watch British TV! Luther is back and as broody as ever in new trailer for fifth season. https://t.co/7FTbP6TNqY",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/7FTbP6TNqY",
+          "expanded_url": "https://apple.news/AB_8fGgoEQ1SA4ek_jxl6nQ",
+          "display_url": "apple.news/AB_8fGgoEQ1SA4…",
+          "indices": [
+            93,
+            116
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  }
+]
