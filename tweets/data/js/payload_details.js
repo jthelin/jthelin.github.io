dd18d5a5ca427e08f88d29686206f5c89c06a321
Jorgen Thelin <jthelin@microsoft.com>
2019-05-04 16:27:40 -0700

    Tweets 2019-05-04

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 1620,
+  "tweets": 1621,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Wed May 01 04:38:02 +0000 2019"
+  "updated_at": "Sat May 04 23:27:40 +0000 2019"
 }
---
+++ tweets/data/js/tweets/2019_05.js
@@ -0,0 +1,57 @@
+Grailbird.data.tweets_2019_05 = [
+  {
+    "created_at": "Wed May 01 16:45:23 +0000 2019",
+    "id": 1123629500661506048,
+    "id_str": "1123629500661506048",
+    "text": "Reproducible builds are a set of software development practices that create an independently-verifiable path from s… https://t.co/7yTlNSZvLH",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/7yTlNSZvLH",
+          "expanded_url": "https://twitter.com/i/web/status/1123629500661506048",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 2,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  }
+]
