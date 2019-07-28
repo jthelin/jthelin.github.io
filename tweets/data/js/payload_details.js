4dea01454979a36404a965c787b58ab9931fb5e6
Jorgen Thelin <jthelin@microsoft.com>
2019-07-28 12:32:16 -0700

    Tweets 2019-07-28

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 1643,
+  "tweets": 1645,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Mon Jul 08 03:49:08 +0000 2019"
+  "updated_at": "Sun Jul 28 19:32:16 +0000 2019"
 }
--- tweets/data/js/tweets/2019_07.js
+++ tweets/data/js/tweets/2019_07.js
@@ -1,5 +1,242 @@
 Grailbird.data.tweets_2019_07 = [
   {
+    "created_at": "Sun Jul 21 17:51:59 +0000 2019",
+    "id": 1152999676590772224,
+    "id_str": "1152999676590772224",
+    "text": "As we remember the incredible achievements of the Apollo 11 moon landings, let's also remember the amazing computin… https://t.co/XBkOeH98UV",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/XBkOeH98UV",
+          "expanded_url": "https://twitter.com/i/web/status/1152999676590772224",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Sat Jul 13 03:05:03 +0000 2019",
+    "id": 1149877372281225216,
+    "id_str": "1149877372281225216",
+    "text": "RT @reubenbond: This request is likely very esoteric, but it would benefit us: https://t.co/1QvwPbhrxL\nWhat are the chances we'll get this…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "reubenbond",
+          "name": "Reuben Bond",
+          "id": 57183972,
+          "id_str": "57183972",
+          "indices": [
+            3,
+            14
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/1QvwPbhrxL",
+          "expanded_url": "https://github.com/dotnet/corefx/issues/37755",
+          "display_url": "github.com/dotnet/corefx/…",
+          "indices": [
+            79,
+            102
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Sat Jul 13 00:07:21 +0000 2019",
+      "id": 1149832652582617088,
+      "id_str": "1149832652582617088",
+      "text": "This request is likely very esoteric, but it would benefit us: https://t.co/1QvwPbhrxL\nWhat are the chances we'll g… https://t.co/StwS96teyl",
+      "truncated": true,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/1QvwPbhrxL",
+            "expanded_url": "https://github.com/dotnet/corefx/issues/37755",
+            "display_url": "github.com/dotnet/corefx/…",
+            "indices": [
+              63,
+              86
+            ]
+          },
+          {
+            "url": "https://t.co/StwS96teyl",
+            "expanded_url": "https://twitter.com/i/web/status/1149832652582617088",
+            "display_url": "twitter.com/i/web/status/1…",
+            "indices": [
+              117,
+              140
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 57183972,
+        "id_str": "57183972",
+        "name": "Reuben Bond",
+        "screen_name": "reubenbond",
+        "location": "Redmond, WA",
+        "description": "Dev on Microsoft Orleans (@msftorleans)",
+        "url": "https://t.co/azrMXKxHTI",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/azrMXKxHTI",
+                "expanded_url": "https://reubenbond.github.io/",
+                "display_url": "reubenbond.github.io",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 2139,
+        "friends_count": 873,
+        "listed_count": 77,
+        "created_at": "Thu Jul 16 00:14:32 +0000 2009",
+        "favourites_count": 5578,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 6891,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "1A1B1F",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": true,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1154759688807780353/7VFZr1mT_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1154759688807780353/7VFZr1mT_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/57183972/1563421030",
+        "profile_link_color": "4C76B2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 3,
+      "favorite_count": 10,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 3,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
     "created_at": "Mon Jul 08 03:07:26 +0000 2019",
     "id": 1148066031736242176,
     "id_str": "1148066031736242176",
