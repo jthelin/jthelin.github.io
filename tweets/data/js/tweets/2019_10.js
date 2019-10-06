cbe45e15eb86eaa6335ec43293c19db7c2826519
Jorgen Thelin <jthelin@microsoft.com>
2019-10-05 17:38:37 -0700

    Tweets 2019-10-05

---
+++ tweets/data/js/tweets/2019_10.js
@@ -0,0 +1,112 @@
+Grailbird.data.tweets_2019_10 = [
+  {
+    "created_at": "Sat Oct 05 00:41:21 +0000 2019",
+    "id": 1180281789840887808,
+    "id_str": "1180281789840887808",
+    "text": "A New Linux Memory Allocator Promises to Save Lots of RAM  https://t.co/A9XMmjSxh9",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/A9XMmjSxh9",
+          "expanded_url": "https://thenewstack.io/a-new-linux-memory-controller-promises-to-save-lots-of-ram/",
+          "display_url": "thenewstack.io/a-new-linux-me…",
+          "indices": [
+            59,
+            82
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Thu Oct 03 22:09:41 +0000 2019",
+    "id": 1179881233020407814,
+    "id_str": "1179881233020407814",
+    "text": "Update your bash prompt to give each hostname a different color. https://t.co/w6f5kfdgPR",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/w6f5kfdgPR",
+          "expanded_url": "https://blog.petdance.com/2019/10/02/bash-color-prompt-hostname/",
+          "display_url": "blog.petdance.com/2019/10/02/bas…",
+          "indices": [
+            65,
+            88
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  }
+]
