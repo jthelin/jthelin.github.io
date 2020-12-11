2d08fde2838f65ca749435ce55d7fa2c8e5c8fa4
jthelin <jthelin@microsoft.com>
2020-12-11 09:59:34 -0800

    Tweets 2020-12-11

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -2,5 +2,5 @@ var payload_details = {
   "tweets": 1971,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Mon Dec 07 00:34:43 +0000 2020"
+  "updated_at": "Fri Dec 11 17:59:34 +0000 2020"
 }
