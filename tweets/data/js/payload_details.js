5c812c5692625389e80510b44159d4d96f92d4e2
Jorgen Thelin <jthelin@microsoft.com>
2021-10-14 12:52:54 -0700

    Tweets 2021-10-07

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 2045,
+  "tweets": 2049,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Fri Oct 01 22:24:39 +0000 2021"
+  "updated_at": "Thu Oct 14 19:52:54 +0000 2021"
 }
---
+++ tweets/data/js/tweets/2021_10.js
@@ -0,0 +1,526 @@
+Grailbird.data.tweets_2021_10 = [
+  {
+    "created_at": "Thu Oct 07 22:47:21 +0000 2021",
+    "id": 1446245790892978216,
+    "id_str": "1446245790892978216",
+    "text": "Social media companies should just switch back to using a Chronological Timeline, rather than letting the algos run… https://t.co/FOWRcwhxZm",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/FOWRcwhxZm",
+          "expanded_url": "https://twitter.com/i/web/status/1446245790892978216",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Thu Oct 07 22:32:29 +0000 2021",
+    "id": 1446242050932764709,
+    "id_str": "1446242050932764709",
+    "text": "RT @reubenbond: This talk is focused on how the RPC &amp; Serializer in Orleans work and how they are designed for high performance. Rather tha…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "reubenbond",
+          "name": "Reuben Bond",
+          "id": 57183972,
+          "id_str": "57183972",
+          "indices": [
+            3,
+            14
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Wed Oct 06 18:10:18 +0000 2021",
+      "id": 1445813680512258049,
+      "id_str": "1445813680512258049",
+      "text": "This talk is focused on how the RPC &amp; Serializer in Orleans work and how they are designed for high performance. Ra… https://t.co/36iR7aIBPV",
+      "truncated": true,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/36iR7aIBPV",
+            "expanded_url": "https://twitter.com/i/web/status/1445813680512258049",
+            "display_url": "twitter.com/i/web/status/1…",
+            "indices": [
+              121,
+              144
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 57183972,
+        "id_str": "57183972",
+        "name": "Reuben Bond",
+        "screen_name": "reubenbond",
+        "location": "Redmond, WA",
+        "description": "Distributed Systems, Databases, Programming Models, Leads Microsoft Orleans @msftorleans",
+        "url": "https://t.co/NqJ0zevu84",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/NqJ0zevu84",
+                "expanded_url": "https://reubenbond.github.io",
+                "display_url": "reubenbond.github.io",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 3849,
+        "friends_count": 1064,
+        "listed_count": 111,
+        "created_at": "Thu Jul 16 00:14:32 +0000 2009",
+        "favourites_count": 8955,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 9123,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "1A1B1F",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": true,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1295042158311829504/RiucKurM_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1295042158311829504/RiucKurM_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/57183972/1571686647",
+        "profile_link_color": "4C76B2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": true,
+      "quoted_status_id": 1445797518994862083,
+      "quoted_status_id_str": "1445797518994862083",
+      "quoted_status": {
+        "created_at": "Wed Oct 06 17:06:04 +0000 2021",
+        "id": 1445797518994862083,
+        "id_str": "1445797518994862083",
+        "text": "😍Lead for Microsoft Orleans @ReubenBond about \"A Deep Dive into Orleans\"\nhttps://t.co/d3ukN6G6YE https://t.co/fHnkUunsvv",
+        "truncated": false,
+        "entities": {
+          "hashtags": [
+
+          ],
+          "symbols": [
+
+          ],
+          "user_mentions": [
+            {
+              "screen_name": "reubenbond",
+              "name": "Reuben Bond",
+              "id": 57183972,
+              "id_str": "57183972",
+              "indices": [
+                28,
+                39
+              ]
+            }
+          ],
+          "urls": [
+            {
+              "url": "https://t.co/d3ukN6G6YE",
+              "expanded_url": "https://www.youtube.com/watch?v=kgRag4E6b4c",
+              "display_url": "youtube.com/watch?v=kgRag4…",
+              "indices": [
+                73,
+                96
+              ]
+            }
+          ],
+          "media": [
+            {
+              "id": 1445797351269048321,
+              "id_str": "1445797351269048321",
+              "indices": [
+                97,
+                120
+              ],
+              "media_url": "http://pbs.twimg.com/media/FBCBB-OXoAEimNX.jpg",
+              "media_url_https": "https://pbs.twimg.com/media/FBCBB-OXoAEimNX.jpg",
+              "url": "https://t.co/fHnkUunsvv",
+              "display_url": "pic.twitter.com/fHnkUunsvv",
+              "expanded_url": "https://twitter.com/konradkokosa/status/1445797518994862083/photo/1",
+              "type": "photo",
+              "sizes": {
+                "thumb": {
+                  "w": 150,
+                  "h": 150,
+                  "resize": "crop"
+                },
+                "large": {
+                  "w": 1280,
+                  "h": 720,
+                  "resize": "fit"
+                },
+                "small": {
+                  "w": 680,
+                  "h": 383,
+                  "resize": "fit"
+                },
+                "medium": {
+                  "w": 1200,
+                  "h": 675,
+                  "resize": "fit"
+                }
+              }
+            }
+          ]
+        },
+        "extended_entities": {
+          "media": [
+            {
+              "id": 1445797351269048321,
+              "id_str": "1445797351269048321",
+              "indices": [
+                97,
+                120
+              ],
+              "media_url": "http://pbs.twimg.com/media/FBCBB-OXoAEimNX.jpg",
+              "media_url_https": "https://pbs.twimg.com/media/FBCBB-OXoAEimNX.jpg",
+              "url": "https://t.co/fHnkUunsvv",
+              "display_url": "pic.twitter.com/fHnkUunsvv",
+              "expanded_url": "https://twitter.com/konradkokosa/status/1445797518994862083/photo/1",
+              "type": "photo",
+              "sizes": {
+                "thumb": {
+                  "w": 150,
+                  "h": 150,
+                  "resize": "crop"
+                },
+                "large": {
+                  "w": 1280,
+                  "h": 720,
+                  "resize": "fit"
+                },
+                "small": {
+                  "w": 680,
+                  "h": 383,
+                  "resize": "fit"
+                },
+                "medium": {
+                  "w": 1200,
+                  "h": 675,
+                  "resize": "fit"
+                }
+              }
+            }
+          ]
+        },
+        "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+        "in_reply_to_status_id": null,
+        "in_reply_to_status_id_str": null,
+        "in_reply_to_user_id": null,
+        "in_reply_to_user_id_str": null,
+        "in_reply_to_screen_name": null,
+        "user": {
+          "id": 1265564497,
+          "id_str": "1265564497",
+          "name": "Konrad Kokosa",
+          "screen_name": "konradkokosa",
+          "location": "Poland",
+          "description": "Author of https://t.co/Wr3HHhOxCB, Microsoft .NET MVP, architect, performance fan, one of @dotnetosorg, author of @outofmemorygame. Helping @unity3d & @plasticscm",
+          "url": "https://t.co/YEyyHa6Mf1",
+          "entities": {
+            "url": {
+              "urls": [
+                {
+                  "url": "https://t.co/YEyyHa6Mf1",
+                  "expanded_url": "https://tooslowexception.com/",
+                  "display_url": "tooslowexception.com",
+                  "indices": [
+                    0,
+                    23
+                  ]
+                }
+              ]
+            },
+            "description": {
+              "urls": [
+                {
+                  "url": "https://t.co/Wr3HHhOxCB",
+                  "expanded_url": "http://prodotnetmemory.com",
+                  "display_url": "prodotnetmemory.com",
+                  "indices": [
+                    10,
+                    33
+                  ]
+                }
+              ]
+            }
+          },
+          "protected": false,
+          "followers_count": 9129,
+          "friends_count": 4786,
+          "listed_count": 167,
+          "created_at": "Wed Mar 13 22:00:24 +0000 2013",
+          "favourites_count": 19210,
+          "utc_offset": null,
+          "time_zone": null,
+          "geo_enabled": false,
+          "verified": false,
+          "statuses_count": 11237,
+          "lang": null,
+          "contributors_enabled": false,
+          "is_translator": false,
+          "is_translation_enabled": false,
+          "profile_background_color": "000000",
+          "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+          "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+          "profile_background_tile": false,
+          "profile_image_url": "http://pbs.twimg.com/profile_images/580093679089643520/oUMnCB7a_normal.jpg",
+          "profile_image_url_https": "https://pbs.twimg.com/profile_images/580093679089643520/oUMnCB7a_normal.jpg",
+          "profile_banner_url": "https://pbs.twimg.com/profile_banners/1265564497/1558433103",
+          "profile_link_color": "3B94D9",
+          "profile_sidebar_border_color": "000000",
+          "profile_sidebar_fill_color": "000000",
+          "profile_text_color": "000000",
+          "profile_use_background_image": false,
+          "has_extended_profile": false,
+          "default_profile": false,
+          "default_profile_image": false,
+          "following": false,
+          "follow_request_sent": false,
+          "notifications": false,
+          "translator_type": "none",
+          "withheld_in_countries": [
+
+          ]
+        },
+        "geo": null,
+        "coordinates": null,
+        "place": null,
+        "contributors": null,
+        "is_quote_status": false,
+        "retweet_count": 20,
+        "favorite_count": 104,
+        "favorited": false,
+        "retweeted": false,
+        "possibly_sensitive": false,
+        "lang": "en"
+      },
+      "retweet_count": 38,
+      "favorite_count": 124,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": true,
+    "quoted_status_id": 1445797518994862083,
+    "quoted_status_id_str": "1445797518994862083",
+    "retweet_count": 38,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "lang": "en"
+  },
+  {
+    "created_at": "Mon Oct 04 19:16:02 +0000 2021",
+    "id": 1445105450114977802,
+    "id_str": "1445105450114977802",
+    "text": "It’s always DNS",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Sat Oct 02 16:17:56 +0000 2021",
+    "id": 1444335853438857217,
+    "id_str": "1444335853438857217",
+    "text": "700,000 deaths in USA from Covid-19. Remember the fallen.",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "en"
+  }
+]
