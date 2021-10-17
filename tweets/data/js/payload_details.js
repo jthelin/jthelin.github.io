86cbe64f19e6d38f026953d7d483e51621fa6f0f
Jorgen Thelin <jthelin@microsoft.com>
2021-10-17 15:58:39 -0700

    Tweets 2021-10-17

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 2049,
+  "tweets": 2051,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Thu Oct 14 19:52:54 +0000 2021"
+  "updated_at": "Sun Oct 17 22:58:39 +0000 2021"
 }
--- tweets/data/js/tweets/2021_10.js
+++ tweets/data/js/tweets/2021_10.js
@@ -1,5 +1,367 @@
 Grailbird.data.tweets_2021_10 = [
   {
+    "created_at": "Sun Oct 17 00:04:29 +0000 2021",
+    "id": 1449526693794750469,
+    "id_str": "1449526693794750469",
+    "text": "RT @soumithchintala: PyTorch co-author Sam Gross (@colesbury) has been working on removing the GIL from Python.\n\nLike...we can start using…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "soumithchintala",
+          "name": "Soumith Chintala",
+          "id": 70831441,
+          "id_str": "70831441",
+          "indices": [
+            3,
+            19
+          ]
+        },
+        {
+          "screen_name": "colesbury",
+          "name": "Sam Gross",
+          "id": 65254738,
+          "id_str": "65254738",
+          "indices": [
+            50,
+            60
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Sat Oct 16 03:53:30 +0000 2021",
+      "id": 1449221941055954944,
+      "id_str": "1449221941055954944",
+      "text": "PyTorch co-author Sam Gross (@colesbury) has been working on removing the GIL from Python.\n\nLike...we can start usi… https://t.co/OdWiD7NbAo",
+      "truncated": true,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+          {
+            "screen_name": "colesbury",
+            "name": "Sam Gross",
+            "id": 65254738,
+            "id_str": "65254738",
+            "indices": [
+              29,
+              39
+            ]
+          }
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/OdWiD7NbAo",
+            "expanded_url": "https://twitter.com/i/web/status/1449221941055954944",
+            "display_url": "twitter.com/i/web/status/1…",
+            "indices": [
+              117,
+              140
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 70831441,
+        "id_str": "70831441",
+        "name": "Soumith Chintala",
+        "screen_name": "soumithchintala",
+        "location": "New York City",
+        "description": "I ❤ research -- multi-modal world models, robots.\nI think deeply about lowering the barrier to use A.I.\nI co-created and lead PyTorch at Facebook A.I.",
+        "url": "https://t.co/PbCPY9bKsw",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/PbCPY9bKsw",
+                "expanded_url": "http://soumith.ch",
+                "display_url": "soumith.ch",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 91545,
+        "friends_count": 476,
+        "listed_count": 1836,
+        "created_at": "Wed Sep 02 00:23:57 +0000 2009",
+        "favourites_count": 1867,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": true,
+        "statuses_count": 2314,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "000000",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/959995586689691648/DAFep10r_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/959995586689691648/DAFep10r_normal.jpg",
+        "profile_link_color": "3B94D9",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "000000",
+        "profile_text_color": "000000",
+        "profile_use_background_image": false,
+        "has_extended_profile": true,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 305,
+      "favorite_count": 1539,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 305,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "lang": "en"
+  },
+  {
+    "created_at": "Sat Oct 16 23:53:18 +0000 2021",
+    "id": 1449523880993771520,
+    "id_str": "1449523880993771520",
+    "text": "RT @DonovanBrown: @jezhumble Crap shipped fast is still crap. Not sure who said that first but very true.",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "DonovanBrown",
+          "name": "Donovan Brown #BlackLivesMatter",
+          "id": 51239568,
+          "id_str": "51239568",
+          "indices": [
+            3,
+            16
+          ]
+        },
+        {
+          "screen_name": "jezhumble",
+          "name": "Jez Humble",
+          "id": 15685575,
+          "id_str": "15685575",
+          "indices": [
+            18,
+            28
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Thu Oct 14 10:40:14 +0000 2021",
+      "id": 1448599520724627464,
+      "id_str": "1448599520724627464",
+      "text": "@jezhumble Crap shipped fast is still crap. Not sure who said that first but very true.",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+          {
+            "screen_name": "jezhumble",
+            "name": "Jez Humble",
+            "id": 15685575,
+            "id_str": "15685575",
+            "indices": [
+              0,
+              10
+            ]
+          }
+        ],
+        "urls": [
+
+        ]
+      },
+      "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "in_reply_to_status_id": 1448318922713821186,
+      "in_reply_to_status_id_str": "1448318922713821186",
+      "in_reply_to_user_id": 15685575,
+      "in_reply_to_user_id_str": "15685575",
+      "in_reply_to_screen_name": "jezhumble",
+      "user": {
+        "id": 51239568,
+        "id_str": "51239568",
+        "name": "Donovan Brown #BlackLivesMatter",
+        "screen_name": "DonovanBrown",
+        "location": "Spring, TX",
+        "description": "Partner Program Manager, #Azure CTO Incubations at @Microsoft but these are my opinions.",
+        "url": "https://t.co/jxoYdoApej",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/jxoYdoApej",
+                "expanded_url": "http://DonovanBrown.com",
+                "display_url": "DonovanBrown.com",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 21042,
+        "friends_count": 460,
+        "listed_count": 577,
+        "created_at": "Fri Jun 26 21:43:22 +0000 2009",
+        "favourites_count": 11689,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": true,
+        "verified": false,
+        "statuses_count": 13554,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "000000",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1405545967269691400/V6zWBIaL_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1405545967269691400/V6zWBIaL_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/51239568/1611356689",
+        "profile_link_color": "000000",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "000000",
+        "profile_text_color": "000000",
+        "profile_use_background_image": false,
+        "has_extended_profile": true,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 2,
+      "favorite_count": 6,
+      "favorited": true,
+      "retweeted": true,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 2,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "lang": "en"
+  },
+  {
     "created_at": "Thu Oct 07 22:47:21 +0000 2021",
     "id": 1446245790892978216,
     "id_str": "1446245790892978216",
