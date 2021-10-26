dbaf05ed0850eb13ee4a437980343aaaa7c45d93
jthelin <jthelin@microsoft.com>
2021-10-26 12:52:56 -0700

    Tweets 2021-10-26

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 2051,
+  "tweets": 2055,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Sun Oct 17 22:58:39 +0000 2021"
+  "updated_at": "Tue Oct 26 19:52:56 +0000 2021"
 }
--- tweets/data/js/tweets/2021_10.js
+++ tweets/data/js/tweets/2021_10.js
@@ -1,5 +1,632 @@
 Grailbird.data.tweets_2021_10 = [
   {
+    "created_at": "Mon Oct 25 23:34:59 +0000 2021",
+    "id": 1452780761292046337,
+    "id_str": "1452780761292046337",
+    "text": "RT @reubenbond: Great talk on building reliable, actor-based systems by @bryan_hunter at @strangeloop_stl https://t.co/dnBK3384HD",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "reubenbond",
+          "name": "Reuben Bond",
+          "id": 57183972,
+          "id_str": "57183972",
+          "indices": [
+            3,
+            14
+          ]
+        },
+        {
+          "screen_name": "bryan_hunter",
+          "name": "Bryan Hunter",
+          "id": 15039603,
+          "id_str": "15039603",
+          "indices": [
+            72,
+            85
+          ]
+        },
+        {
+          "screen_name": "strangeloop_stl",
+          "name": "Strange Loop Conf",
+          "id": 28045502,
+          "id_str": "28045502",
+          "indices": [
+            89,
+            105
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/dnBK3384HD",
+          "expanded_url": "https://www.youtube.com/watch?v=9qUfX3XFi_4",
+          "display_url": "youtube.com/watch?v=9qUfX3…",
+          "indices": [
+            106,
+            129
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Mon Oct 25 13:32:51 +0000 2021",
+      "id": 1452629228533411849,
+      "id_str": "1452629228533411849",
+      "text": "Great talk on building reliable, actor-based systems by @bryan_hunter at @strangeloop_stl https://t.co/dnBK3384HD",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+          {
+            "screen_name": "bryan_hunter",
+            "name": "Bryan Hunter",
+            "id": 15039603,
+            "id_str": "15039603",
+            "indices": [
+              56,
+              69
+            ]
+          },
+          {
+            "screen_name": "strangeloop_stl",
+            "name": "Strange Loop Conf",
+            "id": 28045502,
+            "id_str": "28045502",
+            "indices": [
+              73,
+              89
+            ]
+          }
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/dnBK3384HD",
+            "expanded_url": "https://www.youtube.com/watch?v=9qUfX3XFi_4",
+            "display_url": "youtube.com/watch?v=9qUfX3…",
+            "indices": [
+              90,
+              113
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 57183972,
+        "id_str": "57183972",
+        "name": "Reuben Bond",
+        "screen_name": "reubenbond",
+        "location": "Redmond, WA",
+        "description": "Distributed Systems, Databases, Programming Models, Leads Microsoft Orleans @msftorleans",
+        "url": "https://t.co/NqJ0zevu84",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/NqJ0zevu84",
+                "expanded_url": "https://reubenbond.github.io",
+                "display_url": "reubenbond.github.io",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 3881,
+        "friends_count": 1068,
+        "listed_count": 112,
+        "created_at": "Thu Jul 16 00:14:32 +0000 2009",
+        "favourites_count": 9082,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 9132,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "1A1B1F",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": true,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1295042158311829504/RiucKurM_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1295042158311829504/RiucKurM_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/57183972/1571686647",
+        "profile_link_color": "4C76B2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 8,
+      "favorite_count": 44,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 8,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Mon Oct 25 23:33:22 +0000 2021",
+    "id": 1452780355371470851,
+    "id_str": "1452780355371470851",
+    "text": "MacOS 12 Monterey: The Ars Technica review https://t.co/KTvJ8TohyB",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/KTvJ8TohyB",
+          "expanded_url": "https://arstechnica.com/?post_type=post&p=1806212",
+          "display_url": "arstechnica.com/?post_type=pos…",
+          "indices": [
+            43,
+            66
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Fri Oct 22 23:32:58 +0000 2021",
+    "id": 1451693091904114694,
+    "id_str": "1451693091904114694",
+    "text": "RT @tprstly: J.R.R. Token https://t.co/ziaQc70F9b",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "tprstly",
+          "name": "Theo Beastly",
+          "id": 107938429,
+          "id_str": "107938429",
+          "indices": [
+            3,
+            11
+          ]
+        }
+      ],
+      "urls": [
+
+      ],
+      "media": [
+        {
+          "id": 1450748447318056962,
+          "id_str": "1450748447318056962",
+          "indices": [
+            26,
+            49
+          ],
+          "media_url": "http://pbs.twimg.com/media/FCIYBsTXoAI8hgd.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/FCIYBsTXoAI8hgd.jpg",
+          "url": "https://t.co/ziaQc70F9b",
+          "display_url": "pic.twitter.com/ziaQc70F9b",
+          "expanded_url": "https://twitter.com/tprstly/status/1450748454263738370/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "large": {
+              "w": 1280,
+              "h": 1280,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 680,
+              "h": 680,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 1200,
+              "h": 1200,
+              "resize": "fit"
+            }
+          },
+          "source_status_id": 1450748454263738370,
+          "source_status_id_str": "1450748454263738370",
+          "source_user_id": 107938429,
+          "source_user_id_str": "107938429"
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1450748447318056962,
+          "id_str": "1450748447318056962",
+          "indices": [
+            26,
+            49
+          ],
+          "media_url": "http://pbs.twimg.com/media/FCIYBsTXoAI8hgd.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/FCIYBsTXoAI8hgd.jpg",
+          "url": "https://t.co/ziaQc70F9b",
+          "display_url": "pic.twitter.com/ziaQc70F9b",
+          "expanded_url": "https://twitter.com/tprstly/status/1450748454263738370/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "large": {
+              "w": 1280,
+              "h": 1280,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 680,
+              "h": 680,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 1200,
+              "h": 1200,
+              "resize": "fit"
+            }
+          },
+          "source_status_id": 1450748454263738370,
+          "source_status_id_str": "1450748454263738370",
+          "source_user_id": 107938429,
+          "source_user_id_str": "107938429"
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Wed Oct 20 08:59:19 +0000 2021",
+      "id": 1450748454263738370,
+      "id_str": "1450748454263738370",
+      "text": "J.R.R. Token https://t.co/ziaQc70F9b",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+
+        ],
+        "media": [
+          {
+            "id": 1450748447318056962,
+            "id_str": "1450748447318056962",
+            "indices": [
+              13,
+              36
+            ],
+            "media_url": "http://pbs.twimg.com/media/FCIYBsTXoAI8hgd.jpg",
+            "media_url_https": "https://pbs.twimg.com/media/FCIYBsTXoAI8hgd.jpg",
+            "url": "https://t.co/ziaQc70F9b",
+            "display_url": "pic.twitter.com/ziaQc70F9b",
+            "expanded_url": "https://twitter.com/tprstly/status/1450748454263738370/photo/1",
+            "type": "photo",
+            "sizes": {
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "large": {
+                "w": 1280,
+                "h": 1280,
+                "resize": "fit"
+              },
+              "small": {
+                "w": 680,
+                "h": 680,
+                "resize": "fit"
+              },
+              "medium": {
+                "w": 1200,
+                "h": 1200,
+                "resize": "fit"
+              }
+            }
+          }
+        ]
+      },
+      "extended_entities": {
+        "media": [
+          {
+            "id": 1450748447318056962,
+            "id_str": "1450748447318056962",
+            "indices": [
+              13,
+              36
+            ],
+            "media_url": "http://pbs.twimg.com/media/FCIYBsTXoAI8hgd.jpg",
+            "media_url_https": "https://pbs.twimg.com/media/FCIYBsTXoAI8hgd.jpg",
+            "url": "https://t.co/ziaQc70F9b",
+            "display_url": "pic.twitter.com/ziaQc70F9b",
+            "expanded_url": "https://twitter.com/tprstly/status/1450748454263738370/photo/1",
+            "type": "photo",
+            "sizes": {
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "large": {
+                "w": 1280,
+                "h": 1280,
+                "resize": "fit"
+              },
+              "small": {
+                "w": 680,
+                "h": 680,
+                "resize": "fit"
+              },
+              "medium": {
+                "w": 1200,
+                "h": 1200,
+                "resize": "fit"
+              }
+            }
+          }
+        ]
+      },
+      "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 107938429,
+        "id_str": "107938429",
+        "name": "Theo Beastly",
+        "screen_name": "tprstly",
+        "location": "Scotland",
+        "description": "Carbon Based Lifeform #1 @carbonlifeforms - #gamedev #mmorpg #NFTs #playtoearn #bitcoin",
+        "url": null,
+        "entities": {
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 25763,
+        "friends_count": 1472,
+        "listed_count": 1978,
+        "created_at": "Sun Jan 24 07:27:29 +0000 2010",
+        "favourites_count": 75074,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": true,
+        "verified": true,
+        "statuses_count": 1014,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "C0DEED",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1437889060740141065/5GQLuYLo_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1437889060740141065/5GQLuYLo_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/107938429/1634239728",
+        "profile_link_color": "0084B4",
+        "profile_sidebar_border_color": "FFFFFF",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": true,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 1225,
+      "favorite_count": 6381,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "cy"
+    },
+    "is_quote_status": false,
+    "retweet_count": 1225,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "cy"
+  },
+  {
+    "created_at": "Wed Oct 20 05:29:00 +0000 2021",
+    "id": 1450695526907740160,
+    "id_str": "1450695526907740160",
+    "text": "Things are really bad when you have to change the name of your company to try to escape the historic reputation of your brand!",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "en"
+  },
+  {
     "created_at": "Sun Oct 17 00:04:29 +0000 2021",
     "id": 1449526693794750469,
     "id_str": "1449526693794750469",
