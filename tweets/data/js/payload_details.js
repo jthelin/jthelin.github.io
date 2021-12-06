c38a9226d660c847ac62e47fe67e9444f8bc35ef
Jorgen Thelin <jthelin@microsoft.com>
2021-12-05 16:50:19 -0800

    Tweets 2021-12-05

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 2063,
+  "tweets": 2064,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Wed Nov 17 04:20:39 +0000 2021"
+  "updated_at": "Mon Dec 06 00:50:19 +0000 2021"
 }
--- tweets/data/js/tweets/2021_11.js
+++ tweets/data/js/tweets/2021_11.js
@@ -1,5 +1,220 @@
 Grailbird.data.tweets_2021_11 = [
   {
+    "created_at": "Fri Nov 19 15:02:05 +0000 2021",
+    "id": 1461711381913161735,
+    "id_str": "1461711381913161735",
+    "text": "RT @reubenbond: Supercharging your cloud applications with Orleans @msftorleans @dotnet https://t.co/EshWsBHgKy",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "reubenbond",
+          "name": "Reuben Bond",
+          "id": 57183972,
+          "id_str": "57183972",
+          "indices": [
+            3,
+            14
+          ]
+        },
+        {
+          "screen_name": "msftorleans",
+          "name": "Microsoft Orleans",
+          "id": 3366266218,
+          "id_str": "3366266218",
+          "indices": [
+            67,
+            79
+          ]
+        },
+        {
+          "screen_name": "dotnet",
+          "name": ".NET",
+          "id": 757891106,
+          "id_str": "757891106",
+          "indices": [
+            80,
+            87
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/EshWsBHgKy",
+          "expanded_url": "https://youtu.be/wYXSZ9VAQCM",
+          "display_url": "youtu.be/wYXSZ9VAQCM",
+          "indices": [
+            88,
+            111
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Thu Nov 18 03:59:39 +0000 2021",
+      "id": 1461182287886778375,
+      "id_str": "1461182287886778375",
+      "text": "Supercharging your cloud applications with Orleans @msftorleans @dotnet https://t.co/EshWsBHgKy",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+          {
+            "screen_name": "msftorleans",
+            "name": "Microsoft Orleans",
+            "id": 3366266218,
+            "id_str": "3366266218",
+            "indices": [
+              51,
+              63
+            ]
+          },
+          {
+            "screen_name": "dotnet",
+            "name": ".NET",
+            "id": 757891106,
+            "id_str": "757891106",
+            "indices": [
+              64,
+              71
+            ]
+          }
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/EshWsBHgKy",
+            "expanded_url": "https://youtu.be/wYXSZ9VAQCM",
+            "display_url": "youtu.be/wYXSZ9VAQCM",
+            "indices": [
+              72,
+              95
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 57183972,
+        "id_str": "57183972",
+        "name": "Reuben Bond",
+        "screen_name": "reubenbond",
+        "location": "Redmond, WA",
+        "description": "Distributed Systems, Databases, Programming Models, Leads Microsoft Orleans @msftorleans",
+        "url": "https://t.co/B4yVxRrb7g",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/B4yVxRrb7g",
+                "expanded_url": "https://reubenbond.github.io",
+                "display_url": "reubenbond.github.io",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 4008,
+        "friends_count": 1097,
+        "listed_count": 112,
+        "created_at": "Thu Jul 16 00:14:32 +0000 2009",
+        "favourites_count": 9348,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 9189,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "1A1B1F",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": true,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1295042158311829504/RiucKurM_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1295042158311829504/RiucKurM_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/57183972/1571686647",
+        "profile_link_color": "4C76B2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 27,
+      "favorite_count": 65,
+      "favorited": true,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 27,
+    "favorite_count": 0,
+    "favorited": true,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
     "created_at": "Tue Nov 16 19:31:24 +0000 2021",
     "id": 1460691993353342978,
     "id_str": "1460691993353342978",
