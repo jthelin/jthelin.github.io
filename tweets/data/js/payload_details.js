88670a59a4a42def3906a03f2d5d85de6834e978
jthelin <jthelin@microsoft.com>
2022-01-04 19:31:56 -0800

    Tweets 2022-01-04

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 2066,
+  "tweets": 2068,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Sun Jan 02 07:19:50 +0000 2022"
+  "updated_at": "Wed Jan 05 03:31:56 +0000 2022"
 }
---
+++ tweets/data/js/tweets/2022_01.js
@@ -0,0 +1,112 @@
+Grailbird.data.tweets_2022_01 = [
+  {
+    "created_at": "Tue Jan 04 20:25:24 +0000 2022",
+    "id": 1478462588182679552,
+    "id_str": "1478462588182679552",
+    "text": "... and related ... \nHow to analyze crash dump files on Windows\nhttps://t.co/qPFlILu6Gg",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/qPFlILu6Gg",
+          "expanded_url": "https://docs.microsoft.com/en-us/windows/win32/dxtecharts/crash-dump-analysis",
+          "display_url": "docs.microsoft.com/en-us/windows/…",
+          "indices": [
+            64,
+            87
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+    "in_reply_to_status_id": 1478461979819798529,
+    "in_reply_to_status_id_str": "1478461979819798529",
+    "in_reply_to_user_id": 63211254,
+    "in_reply_to_user_id_str": "63211254",
+    "in_reply_to_screen_name": "JorgenThelin",
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  },
+  {
+    "created_at": "Tue Jan 04 20:22:59 +0000 2022",
+    "id": 1478461979819798529,
+    "id_str": "1478461979819798529",
+    "text": "How to collect user-mode crash dump files on Windows.\nhttps://t.co/ejGUvTzffS",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/ejGUvTzffS",
+          "expanded_url": "https://docs.microsoft.com/en-us/windows/win32/wer/collecting-user-mode-dumps",
+          "display_url": "docs.microsoft.com/en-us/windows/…",
+          "indices": [
+            54,
+            77
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 1,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "en"
+  }
+]
