daff4ee67b3e4c13a9304f6682d9e67a73fd5db5
jthelin <jthelin@microsoft.com>
2022-01-05 18:44:26 -0800

    Tweets 2022-01-05

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 2068,
+  "tweets": 2069,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Wed Jan 05 03:31:56 +0000 2022"
+  "updated_at": "Thu Jan 06 02:44:26 +0000 2022"
 }
--- tweets/data/js/tweets/2022_01.js
+++ tweets/data/js/tweets/2022_01.js
@@ -1,5 +1,59 @@
 Grailbird.data.tweets_2022_01 = [
   {
+    "created_at": "Wed Jan 05 07:02:21 +0000 2022",
+    "id": 1478622883479842819,
+    "id_str": "1478622883479842819",
+    "text": "Intel just announced the most powerful room heaters on the planet …… (check notes) …… oh sorry, they are new laptop… https://t.co/6crb5uq3jh",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/6crb5uq3jh",
+          "expanded_url": "https://twitter.com/i/web/status/1478622883479842819",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "en"
+  },
+  {
     "created_at": "Tue Jan 04 20:25:24 +0000 2022",
     "id": 1478462588182679552,
     "id_str": "1478462588182679552",
