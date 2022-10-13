dfacd9e82fd2ba9adeeb450fa40bc598ba65f35f
Jorgen Thelin <jthelin@microsoft.com>
2022-10-12 17:12:46 -0700

    Tweets 2022-08-17

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 2078,
+  "tweets": 2079,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Fri May 13 20:35:48 +0000 2022"
+  "updated_at": "Thu Oct 13 00:12:46 +0000 2022"
 }
---
+++ tweets/data/js/tweets/2022_08.js
@@ -0,0 +1,48 @@
+Grailbird.data.tweets_2022_08 = [
+  {
+    "created_at": "Wed Aug 17 02:34:34 +0000 2022",
+    "id": 1559730374120337409,
+    "id_str": "1559730374120337409",
+    "text": "47,000 miles",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "en"
+  }
+]
