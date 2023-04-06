562f9173e5d5bc1bade25fd43bcc06811e188e52
jthelin <jthelin@microsoft.com>
2023-04-05 22:03:53 -0700

    Tweets 2023-04-05

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 2089,
+  "tweets": 2090,
   "created_at": "2015-07-17 11:47:19 +0000",
   "lang": "en",
-  "updated_at": "Sun Feb 12 03:03:29 +0000 2023"
+  "updated_at": "Thu Apr 06 05:03:53 +0000 2023"
 }
---
+++ tweets/data/js/tweets/2023_03.js
@@ -0,0 +1,48 @@
+Grailbird.data.tweets_2023_03 = [
+  {
+    "created_at": "Wed Mar 29 21:36:52 +0000 2023",
+    "id": 1641192727860871168,
+    "id_str": "1641192727860871168",
+    "text": "INTC stock went up today because they have a good ppt presentation. Seems legit.",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 63211254,
+      "id_str": "63211254",
+      "name": "Jorgen Thelin",
+      "screen_name": "JorgenThelin",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/479298997782188032/0qcGGAlA_normal.jpeg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "en"
+  }
+]
