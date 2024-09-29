da6add9d90a49c471b1292d180e75b05207d44c5
Jorgen Thelin <jthelin@microsoft.com>
2024-09-28 21:06:53 -0700

    ruby gem pkg version updates (#53)
    gem pkg version updates for ruby v3
    
    * ruby gem pkg version updates to latest
    
    * Updates for ruby v3
    
    * Use ruby version 3.3

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 source 'https://rubygems.org'
+
 gem 'github-pages', '~> 228', group: :jekyll_plugins
 
+gem "csv", "~> 3.3"
+gem "webrick", "~> 1.8"
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# https://help.github.com/en/articles/setting-up-your-github-pages-site-locally-with-jekyll#step-4-build-your-local-jekyll-site
-#
 
-bundle exec jekyll serve
+# https://help.github.com/en/articles/setting-up-your-github-pages-site-locally-with-jekyll#step-4-build-your-local-jekyll-site
+
+unset JEKYLL_GITHUB_TOKEN
+
+/usr/local/opt/ruby/bin/bundle exec jekyll serve
+
